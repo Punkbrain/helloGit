e5e09f52dc1a60cd5149f07315cb57125421e613
PunkBrain <2678054554@qq.com>
2020-10-26 21:11:45 +0800

    提交更新1

--- HelloGit/src/main/java/com/punkbrain/git/HelloGit.java
+++ HelloGit/src/main/java/com/punkbrain/git/HelloGit.java
@@ -4,5 +4,6 @@ package com.punkbrain.git;
 public class HelloGit {
     public static void main(String[] args){
         System.out.println("Hello Git!");
+        System.out.println("更新1");
     }
 }
